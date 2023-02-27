dacd1f015dc17095376ee32d2e81b2f83dcd5817
IckyT007 <ickytuberlp@gmail.com>
2023-02-27 22:32:40 +0100

    Changed todo

--- commands/admin.py
+++ commands/admin.py
@@ -13,7 +13,7 @@ def initAdminCommands(bot, args_dict):
 
     TESTING_GUILD_ID = args_dict["TESTING_GUILD_ID"]
 
-    @bot.slash_command(name="registerrole", description="Registers a Role that can use the Bot", guild_ids=[TESTING_GUILD_ID], default_member_permissions=Permissions(administrator=True))
+    @bot.slash_command(name="registerrole", description="Registers a Role that can use the Bot", guild_ids=[TESTING_GUILD_ID], default_member_permissions=Permissions(administrator=True)) # TODO: add permission system
     async def registerRole(interaction: Interaction, roles: Role = SlashOption(name="roles", description="The Role that should be registered", required=True)):
         await interaction.response.send_message("Role registered")
 
